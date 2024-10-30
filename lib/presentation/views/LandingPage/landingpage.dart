c91f34ee2405358f92b67940418d6f3d66805363
David Orakpo <davidorakpo65@gmail.com>
2024-10-30 16:06:56 +0100

    Silenced desktop view

--- lib/presentation/views/LandingPage/landingpage.dart
+++ lib/presentation/views/LandingPage/landingpage.dart
@@ -18,120 +18,132 @@ class _LandingPageState extends State<LandingPage> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: const Color.fromARGB(255, 60, 89, 112),
-        body: Stack(
-          clipBehavior: Clip.antiAlias,
-          fit: StackFit.expand,
-          alignment: Alignment.bottomCenter,
-          children: [
-            GradientBackground(
-              outterColor: Colors.blue.shade100,
-            ),
-            Align(
-              alignment: Alignment.center,
-              child: SizedBox(
-                // color: Color.fromARGB(255, 60, 89, 112),
-
-                child: Transform.scale(
-                  scale: 1.2,
-                  child: Image.asset(
-                    "assets/images/image_profile.png",
-                    height: screenSize.height * 0.7,
-                    width: screenSize.width * 0.8,
-                    fit: BoxFit.contain,
-                    alignment: Alignment.center,
+        body: !Responsive.isMobile(context)
+            ? const Center(
+                child: Text(
+                  "Desktop View is Coming Soon!",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
                   ),
                 ),
-              ),
-            ),
-            Positioned(
-              bottom: 30,
-              child: ClipPath(
-                clipper: PathPainter(),
-                child: Container(
-                  // alignment: Alignment.center,
-                  padding: const EdgeInsets.all(24),
-                  width: screenSize.width * 0.8,
-                  height: screenSize.height * 0.48,
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
+              )
+            : Stack(
+                clipBehavior: Clip.antiAlias,
+                fit: StackFit.expand,
+                alignment: Alignment.bottomCenter,
+                children: [
+                  GradientBackground(
+                    outterColor: Colors.blue.shade100,
                   ),
-                  child: Stack(children: [
-                    Positioned(
-                      right: -10,
-                      top: 24,
-                      child: SizedBox(
-                        height: 40,
-                        width: 40,
+                  Align(
+                    alignment: Alignment.center,
+                    child: SizedBox(
+                      // color: Color.fromARGB(255, 60, 89, 112),
+
+                      child: Transform.scale(
+                        scale: 1.2,
                         child: Image.asset(
-                          "assets/images/flutter_logo.png",
+                          "assets/images/image_profile.png",
+                          height: screenSize.height * 0.7,
+                          width: screenSize.width * 0.8,
+                          fit: BoxFit.contain,
+                          alignment: Alignment.center,
                         ),
                       ),
                     ),
-                    Align(
-                      alignment: Responsive.isLandScape(context)
-                          ? Alignment.centerLeft
-                          : Alignment.center,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: Responsive.isLandScape(context)
-                            ? CrossAxisAlignment.start
-                            : CrossAxisAlignment.center,
-                        mainAxisSize: MainAxisSize.max,
-                        children: [
-                          const SizedBox(
-                            height: 35,
+                  ),
+                  Positioned(
+                    bottom: 30,
+                    child: ClipPath(
+                      clipper: PathPainter(),
+                      child: Container(
+                        // alignment: Alignment.center,
+                        padding: const EdgeInsets.all(24),
+                        width: screenSize.width * 0.8,
+                        height: screenSize.height * 0.48,
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                        ),
+                        child: Stack(children: [
+                          Positioned(
+                            right: -10,
+                            top: 24,
+                            child: SizedBox(
+                              height: 40,
+                              width: 40,
+                              child: Image.asset(
+                                "assets/images/flutter_logo.png",
+                              ),
+                            ),
                           ),
-                          Text(
-                            "Hi, I am David Orakpo.",
-                            textAlign: Responsive.isLandScape(context)
-                                ? TextAlign.left
-                                : TextAlign.center,
-                            style: const TextStyle(
-                                fontSize: 20,
-                                height: 1.5,
-                                color: Color.fromRGBO(5, 70, 123, 0.6),
-                                decoration: TextDecoration.none),
+                          Align(
+                            alignment: Responsive.isLandScape(context)
+                                ? Alignment.centerLeft
+                                : Alignment.center,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment:
+                                  Responsive.isLandScape(context)
+                                      ? CrossAxisAlignment.start
+                                      : CrossAxisAlignment.center,
+                              mainAxisSize: MainAxisSize.max,
+                              children: [
+                                const SizedBox(
+                                  height: 35,
+                                ),
+                                Text(
+                                  "Hi, I am David Orakpo.",
+                                  textAlign: Responsive.isLandScape(context)
+                                      ? TextAlign.left
+                                      : TextAlign.center,
+                                  style: const TextStyle(
+                                      fontSize: 20,
+                                      height: 1.5,
+                                      color: Color.fromRGBO(5, 70, 123, 0.6),
+                                      decoration: TextDecoration.none),
+                                ),
+                                const SizedBox(
+                                  height: 5,
+                                ),
+                                Text(
+                                  "FLUTTER/XAMARIN DEVELOPER",
+                                  textAlign: Responsive.isLandScape(context)
+                                      ? TextAlign.left
+                                      : TextAlign.center,
+                                  style: const TextStyle(
+                                      fontSize: 28,
+                                      height: 1.5,
+                                      fontWeight: FontWeight.bold,
+                                      color: Color.fromRGBO(5, 70, 123, 1),
+                                      decoration: TextDecoration.none),
+                                ),
+                              ],
+                            ),
                           ),
-                          const SizedBox(
-                            height: 5,
-                          ),
-                          Text(
-                            "FLUTTER/XAMARIN DEVELOPER",
-                            textAlign: Responsive.isLandScape(context)
-                                ? TextAlign.left
-                                : TextAlign.center,
-                            style: const TextStyle(
-                                fontSize: 28,
-                                height: 1.5,
-                                fontWeight: FontWeight.bold,
-                                color: Color.fromRGBO(5, 70, 123, 1),
-                                decoration: TextDecoration.none),
-                          ),
-                        ],
+                          Align(
+                            alignment: Responsive.isLandScape(context)
+                                ? Alignment.bottomRight
+                                : Alignment.bottomCenter,
+                            child: CustomButton(
+                              text: "Learn More!",
+                              textColor: Colors.white,
+                              fontSize: 16,
+                              height: 48,
+                              width: 180,
+                              backgroundColor:
+                                  const Color.fromRGBO(5, 70, 123, 1),
+                              onPressed: () {
+                                Navigator.pushNamed(context, "/homeTabManager");
+                              },
+                            ),
+                          )
+                        ]),
                       ),
                     ),
-                    Align(
-                      alignment: Responsive.isLandScape(context)
-                          ? Alignment.bottomRight
-                          : Alignment.bottomCenter,
-                      child: CustomButton(
-                        text: "Learn More!",
-                        textColor: Colors.white,
-                        fontSize: 16,
-                        height: 48,
-                        width: 180,
-                        backgroundColor: const Color.fromRGBO(5, 70, 123, 1),
-                        onPressed: () {
-                          Navigator.pushNamed(context, "/homeTabManager");
-                        },
-                      ),
-                    )
-                  ]),
-                ),
+                  )
+                ],
               ),
-            )
-          ],
-        ),
       ),
     );
   }
--- lib/presentation/views/home/hometabmanager.dart
+++ lib/presentation/views/home/hometabmanager.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:portfolio_app/core/StateManagement/dark_theme_provider.dart';
+import 'package:portfolio_app/core/utility/responsive/responsive.dart';
 import 'package:portfolio_app/presentation/views/Experience/experience.dart';
 import 'package:portfolio_app/presentation/views/portfolio/portfolio.dart';
 import 'package:portfolio_app/presentation/views/settings/settings.dart';
@@ -37,118 +38,132 @@ class _HomeTabManagerState extends State<HomeTabManager> {
       child: Scaffold(
         resizeToAvoidBottomInset: false,
         backgroundColor: Colors.white,
-        body: Stack(
-          clipBehavior: Clip.none,
-          alignment: Alignment.center,
-          children: [
-            IndexedStack(
-              index: homeTabPageIndex,
-              children: [
-                AboutMePage(),
-                ExperiencePage(),
-                PortfolioPage(),
-                SettingsPage(),
-              ],
-            ),
-            Stack(
-              clipBehavior: Clip.none,
-              alignment: Alignment.center,
-              children: [
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Material(
-                    elevation: 30,
-                    child: Container(
-                      height: bottomTabHeight,
-                      color: Theme.of(context).colorScheme.tertiary,
-                      width: double.infinity,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 12),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Expanded(
-                              child: CustomTabItem(
-                                selectedIndex: homeTabPageIndex,
-                                assignedIndex: 0,
-                                text: "About Me",
-                                iconData: Icons.person_rounded,
-                                selectedColor:
-                                    Theme.of(context).colorScheme.primary,
-                                unSelectedColor:
-                                    Theme.of(context).unselectedWidgetColor,
-                                largeText: true,
-                                onTap: (() {
-                                  setState(() {
-                                    homeTabPageIndex = 0;
-                                  });
-                                }),
-                              ),
-                            ),
-                            Expanded(
-                              child: CustomTabItem(
-                                selectedIndex: homeTabPageIndex,
-                                selectedColor:
-                                    Theme.of(context).colorScheme.primary,
-                                unSelectedColor:
-                                    Theme.of(context).unselectedWidgetColor,
-                                assignedIndex: 1,
-                                text: "Experience",
-                                iconData: Icons.work_history_rounded,
-                                largeText: true,
-                                onTap: (() {
-                                  setState(() {
-                                    homeTabPageIndex = 1;
-                                  });
-                                }),
-                              ),
-                            ),
-                            Expanded(
-                              child: CustomTabItem(
-                                selectedIndex: homeTabPageIndex,
-                                selectedColor:
-                                    Theme.of(context).colorScheme.primary,
-                                unSelectedColor:
-                                    Theme.of(context).unselectedWidgetColor,
-                                assignedIndex: 2,
-                                text: "Portfolio",
-                                iconData: Icons.workspaces_rounded,
-                                largeText: true,
-                                onTap: (() {
-                                  setState(() {
-                                    homeTabPageIndex = 2;
-                                  });
-                                }),
-                              ),
-                            ),
-                            Expanded(
-                              child: CustomTabItem(
-                                selectedIndex: homeTabPageIndex,
-                                assignedIndex: 3,
-                                selectedColor:
-                                    Theme.of(context).colorScheme.primary,
-                                unSelectedColor:
-                                    Theme.of(context).unselectedWidgetColor,
-                                text: "Settings",
-                                iconData: Icons.settings_rounded,
-                                largeText: true,
-                                onTap: (() {
-                                  setState(() {
-                                    homeTabPageIndex = 3;
-                                  });
-                                }),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
+        body: !Responsive.isMobile(context)
+            ? Container(
+                color: const Color.fromARGB(255, 60, 89, 112),
+                child: const Center(
+                  child: Text(
+                    "Desktop View is Coming Soon!",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40,
                     ),
                   ),
-                )
-              ],
-            )
-          ],
-        ),
+                ),
+              )
+            : Stack(
+                clipBehavior: Clip.none,
+                alignment: Alignment.center,
+                children: [
+                  IndexedStack(
+                    index: homeTabPageIndex,
+                    children: [
+                      AboutMePage(),
+                      ExperiencePage(),
+                      PortfolioPage(),
+                      SettingsPage(),
+                    ],
+                  ),
+                  Stack(
+                    clipBehavior: Clip.none,
+                    alignment: Alignment.center,
+                    children: [
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Material(
+                          elevation: 30,
+                          child: Container(
+                            height: bottomTabHeight,
+                            color: Theme.of(context).colorScheme.tertiary,
+                            width: double.infinity,
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 12),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Expanded(
+                                    child: CustomTabItem(
+                                      selectedIndex: homeTabPageIndex,
+                                      assignedIndex: 0,
+                                      text: "About Me",
+                                      iconData: Icons.person_rounded,
+                                      selectedColor:
+                                          Theme.of(context).colorScheme.primary,
+                                      unSelectedColor: Theme.of(context)
+                                          .unselectedWidgetColor,
+                                      largeText: true,
+                                      onTap: (() {
+                                        setState(() {
+                                          homeTabPageIndex = 0;
+                                        });
+                                      }),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: CustomTabItem(
+                                      selectedIndex: homeTabPageIndex,
+                                      selectedColor:
+                                          Theme.of(context).colorScheme.primary,
+                                      unSelectedColor: Theme.of(context)
+                                          .unselectedWidgetColor,
+                                      assignedIndex: 1,
+                                      text: "Experience",
+                                      iconData: Icons.work_history_rounded,
+                                      largeText: true,
+                                      onTap: (() {
+                                        setState(() {
+                                          homeTabPageIndex = 1;
+                                        });
+                                      }),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: CustomTabItem(
+                                      selectedIndex: homeTabPageIndex,
+                                      selectedColor:
+                                          Theme.of(context).colorScheme.primary,
+                                      unSelectedColor: Theme.of(context)
+                                          .unselectedWidgetColor,
+                                      assignedIndex: 2,
+                                      text: "Portfolio",
+                                      iconData: Icons.workspaces_rounded,
+                                      largeText: true,
+                                      onTap: (() {
+                                        setState(() {
+                                          homeTabPageIndex = 2;
+                                        });
+                                      }),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: CustomTabItem(
+                                      selectedIndex: homeTabPageIndex,
+                                      assignedIndex: 3,
+                                      selectedColor:
+                                          Theme.of(context).colorScheme.primary,
+                                      unSelectedColor: Theme.of(context)
+                                          .unselectedWidgetColor,
+                                      text: "Settings",
+                                      iconData: Icons.settings_rounded,
+                                      largeText: true,
+                                      onTap: (() {
+                                        setState(() {
+                                          homeTabPageIndex = 3;
+                                        });
+                                      }),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
       ),
     );
   }
