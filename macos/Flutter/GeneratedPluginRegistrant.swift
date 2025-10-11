314cf65cc346181fb08905c64ca9a16b68cccfff
david@bani.africa <davidorakpo65@gmail.com>
2025-10-11 16:46:01 +0100

    chore: update sqflite to sqflite_darwin and adjust iOS build settings

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,7 +7,7 @@ import Foundation
 
 import path_provider_foundation
 import shared_preferences_foundation
-import sqflite
+import sqflite_darwin
 import url_launcher_macos
 import video_player_avfoundation
 
